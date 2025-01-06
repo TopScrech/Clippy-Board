11c7b78a791b9cc05beca7a7e41e7cdc3d283273
TopScrech <sergei_saliukov@icloud.com>
2025-01-06 01:30:31 +0100

    -

--- Clippy Board/Settings/Settings View.swift
+++ Clippy Board/Settings/Settings View.swift
@@ -1,12 +1,36 @@
 import SwiftUI
 import LaunchAtLogin
+import ServiceManagement
 
 struct SettingsView: View {
     var body: some View {
         VStack {
             LaunchAtLogin.Toggle {
                 Text(LaunchAtLogin.isEnabled ? "Launch at login 🦄" : "Disable launch at login")
+                
+#warning("Show status")
+                //                Text(serviceManagementStatus())
             }
+        }
+        .padding()
+    }
+    
+    private func serviceManagementStatus() -> String {
+        switch SMAppService.mainApp.status {
+        case .notRegistered:
+            "The service hasn’t registered with the Service Management framework, or the service attempted to reregister after it was already registered"
+            
+        case .enabled:
+            "The service has been successfully registered and is eligible to run"
+            
+        case .requiresApproval:
+            "The service has been successfully registered, but the user needs to take action in System Preferences"
+            
+        case .notFound:
+            "An error occurred and the framework couldn’t find this service"
+            
+        default:
+            "Unknown default"
         }
     }
 }
